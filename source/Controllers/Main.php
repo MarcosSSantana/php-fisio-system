14eccf11abf1253e6cbb30554638672a20b405ac
Marcos Santana <marcosshotokan10@gmail.com>
2024-06-20 15:48:28 -0300

    ✨ feat grafico de idades

--- source/Controllers/Main.php
+++ source/Controllers/Main.php
@@ -8,6 +8,7 @@ use Source\Models\Convenio;
 use Source\Models\Paciente;
 use Source\Models\Sessao;
 use CoffeeCode\DataLayer\Connect;
+
 class Main
 {
     private $view;
@@ -19,45 +20,44 @@ class Main
 
     public function inicio($data)
     {
-//        echo "<h1>Inicio</h1>";
+        //        echo "<h1>Inicio</h1>";
         $pacientesMasc = (new Paciente())->find("sexo = :sexo", "sexo=Masculino")->count();
         $pacientesFemi = (new Paciente())->find("sexo = :sexo", "sexo=Feminino")->count();
         $pacientes = (new Paciente())->find()->count();
         $sessoes = (new Sessao())->find()->count();
+
         $obsessesData = (Connect::getInstance(DATA_LAYER_CONFIG))
             ->query("SELECT DATE(created_at) as data, COUNT(*) as quantidade FROM sessao GROUP BY DATE(created_at) ORDER BY data ASC;")
             ->fetchAll(PDO::FETCH_OBJ);
-        //(new Sessao())->find()->order("created_at ASC")->fetch(true);
-        $list = "";//(new Paciente())->find()->fetch(true);
+
+        $arrayIdades = (Connect::getInstance(DATA_LAYER_CONFIG))->query("SELECT idade, COUNT(*) AS quantidade FROM paciente GROUP BY idade ORDER BY idade;")->fetchAll(PDO::FETCH_OBJ);
+        $listaIdades = [];
+        foreach ($arrayIdades as  $value) {
+            $listaIdades['idades'][] = $value->idade;
+            $listaIdades['quantidade'][] = $value->quantidade;
+        }
+
         echo $this->view->render("inicio", [
             "title" => "Inicio",
             "qtdPacientesMasc" => $pacientesMasc,
             "qtdPacientesFemi" => $pacientesFemi,
             "qtdPacientes" => $pacientes,
             "qtdSessoes" => $sessoes,
-            "dataSessoes" => $obsessesData
+            "dataSessoes" => $obsessesData,
+            "listaIdades" => $listaIdades
         ]);
-//        foreach ($list as $item) {
-//            print_r($item->nome);
-//        }
-//
-//        $list= (new Convenio())->find()->fetch(true);
-//
-//        foreach ($list as $item) {
-//            print_r($item->nome);
-//        }
     }
 
     public function logar($data)
     {
-//        echo "login";
-//        echo "<pre>";
-//        print_r($_SESSION);
-//        echo "</pre>";
+        //        echo "login";
+        //        echo "<pre>";
+        //        print_r($_SESSION);
+        //        echo "</pre>";
         echo $this->view->render("login", [
             "title" => "Login"
         ]);
-//
+        //
     }
 
     public function login($data)
@@ -73,7 +73,5 @@ class Main
                 "title" => "Login"
             ]);
         }
-
     }
-
 }
--- source/View/inicio.php
+++ source/View/inicio.php
@@ -4,7 +4,8 @@ $this->layout('_theme', [
 ]);
 ?>
 <pre>
-<!--    --><?php //print_r($this->data["dataSessoes"])?>
+<!--    --><?php //print_r($this->data["dataSessoes"])
+            ?>
 </pre>
 <section class="content">
     <div class="container-fluid">
@@ -15,7 +16,7 @@ $this->layout('_theme', [
 
                 <div class="small-box bg-info">
                     <div class="inner">
-                        <h3><?=$this->data["qtdSessoes"];?></h3>
+                        <h3><?= $this->data["qtdSessoes"]; ?></h3>
                         <p>Sessões</p>
                     </div>
                     <div class="icon">
@@ -29,7 +30,7 @@ $this->layout('_theme', [
 
                 <div class="small-box bg-warning">
                     <div class="inner">
-                        <h3><?=$this->data["qtdPacientes"];?></h3>
+                        <h3><?= $this->data["qtdPacientes"]; ?></h3>
                         <p>Pacientes</p>
                     </div>
                     <div class="icon">
@@ -43,7 +44,7 @@ $this->layout('_theme', [
 
                 <div class="small-box bg-primary">
                     <div class="inner">
-                        <h3><?=$this->data["qtdPacientesMasc"];?></h3>
+                        <h3><?= $this->data["qtdPacientesMasc"]; ?></h3>
                         <p>Sexo Masculino</p>
                     </div>
                     <div class="icon">
@@ -57,7 +58,7 @@ $this->layout('_theme', [
 
                 <div class="small-box bg-maroon">
                     <div class="inner">
-                        <h3><?=$this->data["qtdPacientesFemi"];?></h3>
+                        <h3><?= $this->data["qtdPacientesFemi"]; ?></h3>
                         <p>Sexo Feminino</p>
                     </div>
                     <div class="icon">
@@ -66,6 +67,47 @@ $this->layout('_theme', [
                     <a href="<?= url('paciente') ?>" class="small-box-footer">Mais informações <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
+
+            <div class="col-lg-6 col-12">
+                <canvas id="chartIdades"></canvas>
+            </div>
         </div>
     </div>
 </section>
+<?php $this->push('scripts') ?>
+<script src="<?= ROOT ?>assets/plugins/chart.js/Chart.min.js"></script>
+
+<script>
+    window.addEventListener("load", function(event) {
+        let dados = <?= json_encode($listaIdades) ?>;
+
+        let idades = dados.idades
+        let quantidade = dados.quantidade;
+
+        const ctx = document.getElementById('chartIdades');
+
+        new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: idades,
+                datasets: [{
+                    label: 'Idades pacientes',
+                    data: quantidade,
+                    borderWidth: 5,
+                    fill: false,
+                    borderColor: 'rgb(75, 192, 192)',
+                    tension: 0.1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
+    });
+</script>
+<?php $this->end() ?>
